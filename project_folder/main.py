fbfb804393d9dd1d0c26c6b8d5fa1eceb3e66dd7
Yuriy <vokoramus01@ya.ru>
2021-01-27 18:48:38 +0300

    1

--- project_folder/main.py
+++ project_folder/main.py
@@ -4,6 +4,7 @@
 import random as r
 import pygame
 from os import path
+import csv
 
 r'''
 # Python
@@ -27,11 +28,10 @@ import sys
 print('\n', '=' * 10, datetime.datetime.now(), '=' * 10)
 # ==============================================================================
 
-
 WIDTH, HEIGHT = 900, 600
 FPS = 30
 
-APP_VER = "0.01"
+APP_VER = "0.02"
 APP_NAME = "RSO v" + APP_VER
 
 # colors
@@ -45,7 +45,6 @@ YELLOW = (255, 255, 0)
 # fonts
 font_name = pygame.font.match_font('arial')
 
-
 def draw_text(surf, text, size, x, y, place="center"):
     font = pygame.font.Font(font_name, size)
     text_surface = font.render(text, True, BLUE)
@@ -56,6 +55,14 @@ def draw_text(surf, text, size, x, y, place="center"):
         text_rect.left = x
         text_rect.y = y
     surf.blit(text_surface, text_rect)
+
+def draw_img(img, width, height):
+    bin_img = pygame.image.load(path.join(img_dir, img)).convert()
+    bin_mini_img = pygame.transform.scale(bin_img, (width, height))
+    bin_mini_img.set_colorkey(BLACK)
+    return bin_mini_img
+
+
 
 def show_go_screen():
     # screen.blit(background, background_rect)
@@ -73,20 +80,40 @@ def show_go_screen():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                # pygame.quit()
                 sys.exit()
 
             if last_key == 0:
                 last_key = now
-            if now - last_key > 1000:
+            if now - last_key > 200:
                 if event.type == pygame.KEYUP:
                     last_key = now
                     waiting = False
 
+
+player_data = {}
+with open('data/players.csv') as f:
+    f.readline()
+    reader = csv.reader(f) 
+    for row in reader:
+        player_data[row[0]] = row[1:]
+
+
 def new_player():
-    player = Player(r.choice([k for k in pics.keys()]), 30, 40, WIDTH * 0.50, HEIGHT * 0.50, GREEN)
+    rand_pl = r.choice(list(player_data.keys()))
+    print(rand_pl)
+    player = Player(rand_pl, player_data[rand_pl], 30, 40, WIDTH * 0.50, HEIGHT * 0.50, GREEN)
     all_sprites.add(player)
     return player
+
+def new_bin(id_, x, y):
+    bin = GarbageBin(str(id_), bins[str(id_)], 60, 80, x, y, YELLOW)
+    bins_group.add(bin)
+    all_sprites.add(bin)
+
+def new_act(id_, x, y):
+    act = Action(str(id_), acts[str(id_)], 100, 120, x, y, WHITE)
+    actions_group.add(act)
+    all_sprites.add(act)
 
 
 # Создаем игру и окно
@@ -103,6 +130,24 @@ sound_bad = pygame.mixer.Sound(path.join(snd_dir, '2.wav'))
 sound_water = pygame.mixer.Sound(path.join(snd_dir, '3.mp3'))
 sound_scissors = pygame.mixer.Sound(path.join(snd_dir, '4.mp3'))
 
+# =========== read from CSV =========================
+
+
+bins = {}
+with open('data/bins.csv') as f:  # TODO переместить файл в папку 'data'
+    f.readline()
+    reader = csv.reader(f)
+    for row in reader:
+        bins[row[0]] = row[1:]
+# print(bins)
+
+acts = {}
+with open('data/acts.csv') as f:  # TODO переместить файл в папку 'data'
+    f.readline()
+    reader = csv.reader(f)
+    for row in reader:
+        acts[row[0]] = row[1:]
+# print(acts)
 
 
 # Загрузка всей игровой графики
@@ -110,110 +155,105 @@ img_dir = path.join(path.dirname(__file__), 'img')
 # background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 # background_rect = background.get_rect()
 
-player_img = pygame.image.load(path.join(img_dir, "p1.png")).convert()
-player_mini_img = pygame.transform.scale(player_img, (85, 150))
-# player_mini_img.set_alpha(50)  # transparency
-player_mini_img.set_colorkey(BLACK)
-
+# Попробую назначать img при инициализации Player
+# player_img = {}
+# for i in player_data:
+#     filename = player_data[i][1]
+#     img = pygame.image.load(path.join(img_dir, filename)).convert()
+#     img.set_colorkey(BLACK)
+#     img_small = pygame.transform.scale(img, (75, 75))
+#     player_img(player_data[0]) = player_data[1]
+# 
+# print(player_img)
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, id_, W, H, coordX, coordY, color):
+    def __init__(self, id_, player_data, W, H, coordX, coordY, color=RED):
         super(Player, self).__init__()
-        self.image = pygame.Surface((W, H))
-        # self.image = player_mini_img
-        self.rect = self.image.get_rect()
+
         self.id_ = id_
-        self.color = color
-        self.redraw(self.color)
+        self.img = player_data[0]
+        self.fit_acts = self.fit_seq(player_data[1])
+        self.bonus_acts = player_data[2]
+        self.fit_bin = player_data[3]
+
+        player_img = pygame.image.load(path.join(img_dir, self.img)).convert()
+        player_mini_img = pygame.transform.scale(player_img, (65, 110))
+        player_mini_img.set_colorkey(BLACK)
+        self.image = player_mini_img
+
+        self.rect = self.image.get_rect()
         self.rect.center = (coordX, coordY)
-         # individual (import from diff module)
-        self.fit_acts = ['act 3', 'act 2', 'act 1']
-        self.fit_bin = 'bin 2'
-
-
-    def redraw(self, color):
-        self.image.fill(color)
-        draw_text(self.image, str(self.id_), 18, self.rect.w / 2, self.rect.h / 2)
+        
+    def fit_seq(self, acts):
+        res = acts.split('_')
+        print(res)
+        return res
+        
 
 
 class GarbageBin(pygame.sprite.Sprite):
-    def __init__(self, id_, W, H, coordX, coordY, color):
+    def __init__(self, id_, bins, W, H, coordX, coordY, color=YELLOW):
         super(GarbageBin, self).__init__()
-        # pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((W, H))
-        self.rect = self.image.get_rect()
-        # collide_rect_ratio(0.75) ??????????????????????
-
         self.id_ = id_
-        self.color = color
-        self.redraw(self.color)
-        self.rect.center = (coordX, coordY)
-        self.recolor_time = pygame.time.get_ticks()
+        self.img1 = bins[0]
+        self.img2 = bins[1]
+        self.image = draw_img(self.img1, 55, 80)
 
-    def redraw(self, color):
-        self.image.fill(color)
-        draw_text(self.image, str(self.id_), 18, self.rect.w / 2, self.rect.h / 2)
+        # ДЛЯ ОТЛАДКИ:
+#         self.image = pygame.Surface((W, H))
+#         self.image.fill(color)
+        
+        self.rect = self.image.get_rect()
+        self.rect.center = (coordX, coordY)
+
+        # НАДПИСЬ НА ИЗОБРАЖЕНИИ
+#         draw_text(image, str(self.id_), 18, self.rect.w / 2, self.rect.h / 2)
+
+##===========
 
     def update(self):
-        if self.color in (RED, GREEN) and pygame.time.get_ticks() - self.recolor_time > 1000:
-            # self.color = YELLOW ??
-            self.redraw(YELLOW)
-            self.recolor_time = pygame.time.get_ticks()
+        pass
+##        if self.color in (RED, GREEN) and pygame.time.get_ticks() - self.recolor_time > 1000:
+##            # self.color = YELLOW ??
+##            self.redraw(YELLOW)
+##            self.recolor_time = pygame.time.get_ticks()
 
 
 class Action(pygame.sprite.Sprite):
-    def __init__(self, id_, W, H, coordX, coordY, color):
+    def __init__(self, id_, acts, W, H, coordX, coordY, color=RED):
         super(Action, self).__init__()
         self.id_ = id_
+        self.img1 = acts[0]
+        self.img2 = acts[1]
+        self.sound = pygame.mixer.Sound(path.join(snd_dir, acts[2]))
 
-        # self.image = pygame.Surface((W, H))
-
-        self.filename = pics[self.id_][0]  # заменить на нормальный вариант, сейчас откусываю последний символ
-        # filename = '1.png'  # заменить на нормальный вариант, сейчас откусываю последний символ
-        print(0, self.filename, type(self.filename))
-        t = path.join(img_dir, self.filename)
-        print(t)
-        img = pygame.image.load(path.join(img_dir, self.filename)).convert()
-        self.coordX = coordX
-        self.coordY = coordY
-        self.redraw(self.coordX,self.coordY)
-
-        # self.image = pygame.transform.scale(img, (120, 120))
-        # self.rect = self.image.get_rect()
-
-        # self.color = color
-        # self.redraw(self.color)
-
-    # def redraw(self, color):
-    def redraw(self, coordX, coordY):
-        img = pygame.image.load(path.join(img_dir, self.filename)).convert()
-        self.image = pygame.transform.scale(img, (120, 120))
+        self.image = draw_img(self.img1, 100, 150)
+                
         self.rect = self.image.get_rect()
         self.rect.center = (coordX, coordY)
 
 
-
-        # self.image.fill(color)
-        # draw_text(self.image, str(self.id_), 18, self.rect.w / 2, self.rect.h / 2)
-
-ids =[1, 2, 3]
-
-pics = {}
-pics['d1'] = ['1.png', '1-1.png']
-pics['d2'] = ['2.png', '2-1.png']
-pics['d3'] = ['3.png', '3-1.png']
-
-N_of_BINS = 10
-bins = ['bin ' + str(r.randint(1, 3)) for _ in range(N_of_BINS)]
-N_of_ACTS = 10
-actions = ['d' + str(r.randint(1, 3)) for _ in range(N_of_ACTS)]
-
 hit_bin, hit_act = 0, 0
 selected_rect = None
-bins_seq = []
-actions_seq = []
+ 
+actions_seq = [] # Последовательность произведенных операций с элементом
 no_collisions = True
 bin_tryed = None
+
+
+all_sprites = pygame.sprite.Group()
+bins_group = pygame.sprite.Group() 
+actions_group = pygame.sprite.Group()
+
+new_bin(1,  WIDTH * 0.9, HEIGHT * 0.20)
+new_bin(2,  WIDTH * 0.9, HEIGHT * 0.40)
+new_bin(3,  WIDTH * 0.9, HEIGHT * 0.60)
+
+new_act(1, WIDTH * 0.10, HEIGHT * 0.90)
+new_act(2, WIDTH * 0.30, HEIGHT * 0.90)
+new_act(3, WIDTH * 0.50, HEIGHT * 0.90)
+
+player1 = new_player()
 
 
 settings_screen = True
@@ -224,44 +264,8 @@ while running:
         show_go_screen()  # Entering to settings_screen
         settings_screen = False
 
-        all_sprites = pygame.sprite.Group()
-
         # returning from settings_screen
-        bin1 = GarbageBin('bin 2', 60, 80, WIDTH * 0.9, HEIGHT * 0.20, YELLOW)
-        bin2 = GarbageBin(r.choice(bins), 60, 80, WIDTH * 0.9, HEIGHT * 0.40, YELLOW)
-        bin3 = GarbageBin(r.choice(bins), 60, 80, WIDTH * 0.9, HEIGHT * 0.60, YELLOW)
-        bin4 = GarbageBin(r.choice(bins), 60, 80, WIDTH * 0.9, HEIGHT * 0.80, YELLOW)
 
-        bins_group = pygame.sprite.Group()
-        bins_group.add(bin1)
-        bins_group.add(bin2)
-        bins_group.add(bin3)
-        bins_group.add(bin4)
-
-        all_sprites.add(bin1)
-        all_sprites.add(bin2)
-        all_sprites.add(bin3)
-        all_sprites.add(bin4)
-
-
-
-        act1 = Action('d1', 100, 120, WIDTH * 0.10, HEIGHT * 0.90, WHITE)
-        act2 = Action('d2', 100, 120, WIDTH * 0.30, HEIGHT * 0.90, WHITE)
-        # act3 = Action('d3', 100, 120, WIDTH * 0.50, HEIGHT * 0.90, WHITE)
-        # act4 = Action(str(r.choice(actions)), 100, 120, WIDTH * 0.70, HEIGHT * 0.90, WHITE)
-
-        actions_group = pygame.sprite.Group()
-        actions_group.add(act1)
-        actions_group.add(act2)
-        # actions_group.add(act3)
-        # actions_group.add(act4)
-
-        all_sprites.add(act1)
-        all_sprites.add(act2)
-        # all_sprites.add(act3)
-        # all_sprites.add(act4)
-
-        player1 = new_player()
 
     # ================ end of returning from settings_screen
 
@@ -276,24 +280,32 @@ while running:
 
         # BINS collision checking
     hits_bins = pygame.sprite.spritecollide(player1, bins_group, False)
-    for b in hits_bins:
-        bin_tryed = b.id_
-        # ДОБАВИТЬ: подсветка выбранной корзины (или открывается крышка бака)
-        print(2, b.id_, actions_seq)
-        # ДОБАВИТЬ: если действие - отпускание кнопки мыши
-        if actions_seq == player1.fit_acts and b.id_ == player1.fit_bin:
-            player1.kill()
-            actions_seq.clear()
-            selected_rect = None
-            player1 = new_player()
-            b.color = GREEN
-            b.redraw(b.color)
-            sound_ok.play()
-        else:
-            b.color = RED
-            b.redraw(b.color)
-            actions_seq.clear()
-            sound_bad.play()
+    if no_collisions:  # чтобы фиксировать только переход, а не в течение всего времени нахождения над картинки действия
+        for b in bins_group:
+            b.image = draw_img(b.img1, b.rect.w, b.rect.h)  # рисуем бак с закрытой крышкой
+
+        for hit in hits_bins:
+            bin_tryed = hit.id_
+            hit.image = draw_img(hit.img2, hit.rect.w, hit.rect.h)  # рисуем бак с открытой крышкой
+
+            print('=============')
+            print(hit.id_, player1.fit_bin)
+            print(actions_seq, player1.fit_acts)
+
+            # ДОБАВИТЬ: если действие == отпускание кнопки мыши
+            if actions_seq == player1.fit_acts and hit.id_ == player1.fit_bin:
+                player1.kill()
+                actions_seq.clear()
+                selected_rect = None
+                player1 = new_player()
+##                b.color = GREEN
+##                b.redraw(b.coordX, b.coordY)
+                sound_ok.play()
+            else:
+##                b.color = RED
+##                b.redraw(b.coordX, b.coordY)
+                actions_seq.clear()
+                sound_bad.play()
 
 
         # ACTIONS collision checking
@@ -302,23 +314,21 @@ while running:
 
     if no_collisions:  # чтобы фиксировать только переход, а не в течение всего времени нахождения над картинки действия
         for a in actions_group:
-            a.filename = pics[a.id_][0]
-            a.redraw(a.coordX, a.coordY)
+            a.image = draw_img(a.img1, a.rect.w, a.rect.h)
 
         for hit in hits_actions:
-            hit.filename = pics[hit.id_][1]
-            hit.redraw(hit.coordX, hit.coordY)
-            if hit.id_ == 'd1':
-                sound_water.play()  # СДЕЛАТЬ! индивидуальные звуки для каждого действия
-            else:
-                sound_scissors.play()
+#             print(hit)
+#             print(hit.sound)
+            hit.image = draw_img(hit.img2, hit.rect.w, hit.rect.h)
+            hit.sound.play()
 
+            # составление списка последовательности действий             
             if len(actions_seq) == 0:
                 actions_seq.append(hit.id_)
             else:
                 if hit.id_ != actions_seq[-1]:
                     actions_seq.append(hit.id_)
-                    print(1, actions_seq, actions_seq == player1.fit_acts)
+#                     print(1, actions_seq, actions_seq == player1.fit_acts)
 
 
     no_collisions = True if not hits_bins and not hits_actions else False
@@ -341,6 +351,7 @@ while running:
         elif event.type == pygame.MOUSEBUTTONUP:
             selected_rect = None  # De-select the rect.
             # player1.redraw(color=GREEN)
+
         # Moving the player
         elif event.type == pygame.MOUSEMOTION:
             if selected_rect is not None:
